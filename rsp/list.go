6ba809dd762a3c712e267861d16656cda8d79e6b
uauteam <uauteam.com@gmail.com>
2019-07-31 21:35:38 +0800

    Add response struct

---
+++ rsp/list.go
@@ -0,0 +1,11 @@
+package rsp
+
+
+type ListResponse struct {
+	Elements []interface{} `json:"elements"`
+}
+
+func OfList(elements []interface{})(r ListResponse) {
+	r.Elements = elements
+	return r
+}
---
+++ rsp/page.go
@@ -0,0 +1,17 @@
+package rsp
+
+
+type PageResponse struct {
+	Page uint
+	Size uint
+	Total uint
+	Elements []interface{} `json:"elements"`
+}
+
+func OfPage(page, size, total uint, elements []interface{})(r PageResponse) {
+	r.Page = page
+	r.Size = size
+	r.Total = total
+	r.Elements = elements
+	return r
+}
