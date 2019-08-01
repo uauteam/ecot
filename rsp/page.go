f02a68e874577d8a61c2ae9f3cd76224bb316f6b
uauteam <uauteam.com@gmail.com>
2019-08-01 11:07:02 +0800

    Ignore nil fields

--- rsp/page.go
+++ rsp/page.go
@@ -3,15 +3,15 @@ package rsp
 
 type PageResponse struct {
 	ListResponse
-	Page uint `json:"page"`
-	Size uint `json:"size"`
-	Total uint `json:"total"`
+	Page *uint `json:"page,omitempty"`
+	Size *uint `json:"size,omitempty"`
+	Total *uint `json:"total,omitempty"`
 }
 
 func OfPage(page, size, total uint, elements interface{})(r PageResponse) {
-	r.Page = page
-	r.Size = size
-	r.Total = total
+	r.Page = &page
+	r.Size = &size
+	r.Total = &total
 	r.Elements = elements
 	return r
 }
